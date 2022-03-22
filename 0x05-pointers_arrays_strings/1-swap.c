703e27cd7c2679e65b7487a00e289391fc8ef7b8
Oyindamola <oyindafalade22@gmail.com>
2022-03-22 14:12:37 -0700

    my commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,17 +3,20 @@
 /**
 * swap_int - function that swaps the values of two integers.
 *
-*@a: number 1.
+*@a: numbere 1.
+*
+* @a: number 1.
+*
 *@b: number 2.
 *
-* Return: 0.
+*Return: 0.
 */
 
-void swap_int(int *a , int *b)
+void swap_int(int *a, int *b)
 {
 int c;
 
-c = *a;
-*a= *b;
-*b =c;
+c= *a;
+*a = *b;
+*b = *c;
 }
